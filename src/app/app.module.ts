271149779c4adbd694b18a1e78f66a06e1020914
andreipivoda <andrei.pivoda@gmail.com>
2017-03-01 16:02:52 +0200

    a working upload interface -demo-

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload';
 import { AppComponent } from './app.component';
+import { FileUploadComponent } from './file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FileUploadComponent,
+    FileSelectDirective,
+    FileDropDirective
   ],
   imports: [
     BrowserModule,
---
+++ src/app/file-upload/file-upload.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { FileUploader } from 'ng2-file-upload';
+import { NgClass, NgStyle } from '@angular/common';
+
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
+
+@Component({
+  selector: 'app-file-upload',
+  templateUrl: './file-upload.component.html'
+
+})
+
+export class FileUploadComponent {
+  public uploader: FileUploader = new FileUploader({ url: URL });
+  public hasBaseDropZoneOver: boolean = false;
+  public hasAnotherDropZoneOver: boolean = false;
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  public fileOverAnother(e: any): void {
+    this.hasAnotherDropZoneOver = e;
+  }
+}
